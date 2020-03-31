817722ae55ebf6f297ff9d4c7fcdaa387c464343
susilo hartomo <encusilo545110@gmail.com>
2020-03-31 11:19:10 +0700

    release 1

--- index.js
+++ index.js
@@ -29,38 +29,57 @@ function numberToWords(number) {
 
   var hasil = ''
 
-  while (number > 0) {
-    if (satuan[number]) {
-      hasil += satuan[number]
-      number -= satuan[number]
-    }
-    for (let i = 0; i < nilai.length; i++) {
+  if (number == 0) {
+    return ''
+  }else if (number >= 1000000) {
+    let temp = Math.trunc(number / 1000000)
+    let temp1 = temp * 1000000
+    number -= temp1
+    return `${numberToWords(temp)}juta ${numberToWords(number)}`
+  } else if (number >= 1000) {
+    let temp = Math.trunc(number / 1000)
+    let temp1 = temp * 1000
+    number -= temp1
+    return `${numberToWords(temp)}ribu ${numberToWords(number)}`
+  } else {
+    while (number > 0) {
       if (satuan[number]) {
-        hasil += satuan[number]
-        number -= satuan[number]
-      } else if (number > 0) {
-        let temp = Math.trunc(number / nilai[i][0])
-        let temp1 = temp * nilai[i][0]
-        if (temp !== 0) {
-          if (temp == 1) {
-            if (satuan[temp1]) {
-              hasil += satuan[temp]
+        hasil += `${satuan[number]}`
+        number = 0
+      }
+      for (let i = 0; i < nilai.length; i++) {
+        if (satuan[number]) {
+          hasil += `${satuan[number]}`
+          number = 0
+        } else if (number > 0) {
+          let temp = Math.trunc(number / nilai[i][0])
+          let temp1 = temp * nilai[i][0]
+          number -= temp1
+          if (temp !== 0) {
+            if (temp == 1) {
+              if (satuan[temp1]) {
+                hasil += `${satuan[temp]} `
+              } else {
+                if (nilai[i][0] == 1000000) {
+                  hasil += ` ${satuan[temp]} ${nilai[i][2]} `
+                } else {
+                  hasil += `se${nilai[i][2]} `
+                }
+              }
             } else {
-              hasil += `se${nilai[i][2]} `
-            }
-          } else {
-            if (satuan[temp1]) {
-              hasil += satuan[temp]
-            } else {
-              hasil += `${satuan[temp]} ${nilai[i][2]} `
+              if (satuan[temp1]) {
+                hasil += `${satuan[temp]} `
+              } else {
+                hasil += `${satuan[temp]} ${nilai[i][2]} `
+              }
             }
           }
         }
-        number -= temp1
       }
+      return `${hasil} ${numberToWords(number)}`
     }
+
   }
-  return hasil
 }
 
 // Driver code
@@ -68,8 +87,8 @@ console.log(numberToWords(705));
 console.log(numberToWords(111));
 console.log(numberToWords(72));
 console.log(numberToWords(423));
-// console.log(numberToWords(1000000));
-// console.log(numberToWords(2011845));
+console.log(numberToWords(1000000));
+console.log(numberToWords(2011845));
 
 //abaikan code dibawah ini
 module.exports = numberToWords;
