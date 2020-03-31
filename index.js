e1e372018d90a454e20b8bff163bc97d6bf280e4
riolly <albert.r.cahyantara@gmail.com>
2020-03-31 15:42:44 +0700

    Release 2: 999.999.999.999.999

--- index.js
+++ index.js
@@ -12,30 +12,49 @@ function numberToWords(number) {
     10: ["sepuluh "]
   }
   let unit = [
+    ['triliun '],
+    ['milyar '],
+    ['juta '],
+    ['ribu '],
     ['ratus '],
     ['puluh ', 'belas '],
     ['satuan']
   ];
 
-  let devider = 100;
+  let unitNumber = [
+    1000000000000,
+    1000000000,
+    1000000,
+    1000,
+    100,
+    10,
+    1
+  ];
+
   let i = 0;
+  let devider = unitNumber[i];
   while (devider > number) { // Cari pembagi paling besar yang mungkin
-    devider /= 10;
     i++;
+    devider = unitNumber[i];
   }
 
   let string = '';
   if (number > 0) { // base case number = 0 (habis)
     let numberHead = Math.floor(number / devider); // Cari angka depan
-    if (unit[i][0] != 'satuan') { // Jika bukan satuan
-      if (number < 20 && number > 10) {
+
+    if (unit[i][0] != 'satuan') { 
+      if (number < 20 && number > 10) { // handle belasan
         string = literal[number % devider][0] + unit[i][1];
         number -= Number(String(numberHead) + (number % devider));
       } else {
-        string = literal[numberHead][0] + unit[i][0]; // Tambahkan unit dibelakang angka depan
+        if (literal[numberHead] == undefined) { // rekursi lagi, jika angka depan masih bisa dibagi unit
+          string = numberToWords(numberHead) + unit[i][0];
+        } else {
+          string = literal[numberHead][0] + unit[i][0];
+        }
         number %= devider;
       }
-    } else { // Jika satuan
+    } else {
       if (numberHead == 1) { // Jika angka 1, se => satu
         string = literal[numberHead][1];
       } else {
@@ -43,21 +62,29 @@ function numberToWords(number) {
       }
       number -= numberHead; // number = 0;
     }
-    return string + numberToWords(number);
+
+    if (string == 'setriliun ') { // handle se => satu
+      string = 'satu triliun ';
+    } else if (string == 'semilyar ') {
+      string = 'satu milyar ';
+    } else if (string == 'sejuta ') {
+      string = 'satu juta ';
+    }
+
+    return string + numberToWords(number); // rekursi
   } else {
     return '';
   }
 }
 
 // Driver code
-for (i = 1; i < 1000; i++) {
-  console.log(numberToWords(i))
-  if (i % 100 == 0) {
-    console.log();
-  }
-}
-// console.log(numberToWords(1000000));
-// console.log(numberToWords(2011845));
+// JANGAN DICOBA!!! 
+// for (let asik = 1; asik < 1000000000000; asik++) {
+//   console.log(numberToWords(asik));
+// }
+console.log(numberToWords(705));
+console.log(numberToWords(1000000));
+console.log(numberToWords(2011845));
 
 //abaikan code dibawah ini
 module.exports = numberToWords;
