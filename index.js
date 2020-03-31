678d32811be22dceedbb7de7a79e07b630f5beb3
Xaber <getar.metal@gmail.com>
2020-03-31 22:08:35 +0700

    numbers-in-words first commit

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 function numberToWords(number) {
   // Your code here
+  var huruf = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+
+  if (number == 0) {
+    return "";
+
+  } else if (number < 12 & number != 0) {
+      return huruf[number];
+
+  } else if (number < 20) {
+      return numberToWords(number - 10) + " Belas ";
+
+  } else if (number < 100) {
+      return numberToWords(Math.floor(number / 10)) + " Puluh " + numberToWords(number % 10);
+
+  } else if (number < 200) {
+      return " Seratus " + numberToWords(number - 100);
+
+  } else if (number < 1000) {
+      return numberToWords(Math.floor(number / 100)) + " Ratus " + numberToWords(number % 100);
+
+  } else if (number < 2000) {
+      return " Seribu " + numberToWords(number - 1000);
+
+  } else if (number < 1000000) {
+     return numberToWords(Math.floor(number / 1000)) + " Ribu " + numberToWords(number % 1000);
+
+  } else if (number < 1000000000) {
+      return numberToWords(Math.floor(number / 1000000)) + " Juta " + numberToWords(number % 1000000);
+
+  } else if (number < 1000000000000) {
+      return numberToWords(Math.floor(number / 1000000000)) + " Miliar " + numberToWords(number % 1000000000);
+
+  } else if (number < 1000000000000000) {
+      return numberToWords(Math.floor(number / 1000000000000)) + " Triliun " + numberToWords(number % 1000000000000);
+
+  } else if (number > 1000000000000000) {
+      return "Kegedean";
+  }
+
 }
 
 // Driver code
 console.log(numberToWords(705));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
+console.log(numberToWords(81123));
 
 //abaikan code dibawah ini
 module.exports = numberToWords;
