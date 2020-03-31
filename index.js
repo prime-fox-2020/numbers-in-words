1f97e2144d0694d9db41221f69764bbddc5aba33
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-03-31 22:22:11 +0700

    finished file index.js

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function numberToWords(number) {
-  // Your code here
+function numberToWords(num) {
+  let words = ["", " satu ", " dua ", " tiga ", " empat ", " lima ", "enam ", " tujuh ", " delapan ", " sembilan ", " sepuluh ", " sebelas "];
+
+  if (num < 12) {
+    return words[num];
+  }
+  else if (num <= 19) {
+    return numberToWords(num - 10 ) + " belas";
+  }
+  else if (num < 100) {
+    return numberToWords(num - 10) + " puluh" + numberToWords(num %10);
+  }
+  else if (num > 99 && num < 200) {
+    return "seratus" + numberToWords(num - 100);
+  }
+  else if (num < 1000) {
+    return words[Math.floor(num/100)] + " ratus" + numberToWords(num % 100);
+  }
+  else if (num > 999 && num < 2000){
+    return "seribu" + numberToWords(num - 1000);
+  }
+  else if (num < 1000000) {
+    return numberToWords(Math.floor(num/1000)) + " ribu" + numberToWords(num % 1000);
+  }
+  else if (num < 1000000000) {
+    return numberToWords(Math.floor(num/1000000)) + " juta" + numberToWords(num % 1000000);
+  }
+  else if (num < 1000000000000) {
+    return numberToWords(Math.floor(num/1000000000)) + " miliar" + numberToWords(num % 1000000000);
+  }
+  else if (num < 1000000000000000) {
+    return numberToWords(Math.floor(num % 1000000000000)) + " trilliun" + numberToWords(num % 1000000000000);
+  }
 }
 
 // Driver code
